4c55405e57f2f149c1bae36b2d7c676dacf1a07b
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-06-13 23:25:41 +0700

    create Login screen

--- App.jsx
+++ App.jsx
@@ -7,21 +7,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Landing from "./src/screens/Landing";
 import Login from "./src/screens/Login";
+import Register from "./src/screens/Register";
+import Payment from "./src/screens/Payment";
 
 const Stack = createStackNavigator();
 
 function App(props) {
-
-
-
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Landing"
+        initialRouteName="Payment"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Landing" component={Landing} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Payment" component={Payment} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,33 +1,49 @@
 import React, { Component } from "react";
 import { StatusBar } from "expo-status-bar";
 import { TailwindProvider } from "tailwindcss-react-native";
-import { Text, View, Image, Pressable } from "react-native";
-import Logo from "../../assets/img/aksa-logo.png";
+import { Text, View, Pressable, TextInput } from "react-native";
 
 class Landing extends Component {
   render() {
     return (
       <TailwindProvider>
-      <StatusBar style="auto" />
-      <View className="flex-1 items-center justify-center w-4/5 mx-[10%]">
-        <Image source={Logo} className="w-full h-40 mb-60" />
-        <Text className="text-[38px] font-bold">Find your games</Text>
-
-        <Pressable
-          className="w-full p-4 rounded-md bg-blue-500 hover:bg-blue-300 active:bg-blue-300 mt-[49px]"
-          title="Landing"
-          onPress={() => this.props.navigation.navigate("Landing")}
-        >
-          <Text
-            style={{ fontFamily: "Poppins" }}
-            className="text-white text-center text-2xl"
+        <StatusBar style="auto" />
+        <View className="flex-1 items-center justify-center w-4/5 mx-[10%]">
+          <Text className="text-5xl font-bold mb-12">Masuk</Text>
+          <TextInput
+            placeholder="Username"
+            placeholderTextColor={"#333"}
+            className="border-b border-slate-500 w-full py-3 px-2"
+          />
+          <TextInput
+            secureTextEntry={true}
+            placeholder="Password"
+            placeholderTextColor={"#333"}
+            className="border-b border-slate-500 w-full py-3 px-2 mt-7"
+          />
+          <Pressable
+            className="w-full p-4 rounded-md bg-blue-500 hover:bg-blue-300 active:bg-blue-300 mt-[49px]"
+            title="Landing"
+            onPress={() => this.props.navigation.navigate("Landing")}
           >
-            Mulai
+            <Text
+              style={{ fontFamily: "Poppins" }}
+              className="text-white text-center text-2xl"
+            >
+              Masuk
+            </Text>
+          </Pressable>
+          <Text className="mt-6">
+            Belum punya akun?{" "}
+            <Text
+              onPress={() => this.props.navigation.navigate("Register")}
+              className="text-blue-600 font-medium"
+            >
+              Daftar
+            </Text>
           </Text>
-        </Pressable>
-      </View>
-    </TailwindProvider>
-
+        </View>
+      </TailwindProvider>
     );
   }
 }
