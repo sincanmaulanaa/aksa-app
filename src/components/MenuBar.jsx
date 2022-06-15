33ef88e8fa7ec06fdebe40d8f9c4eea96026daca
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-06-15 19:30:40 +0700

    creates PurchaseHistory screen

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,39 +1,63 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { View, Text, Pressable } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
-import Icon from "react-native-vector-icons/SimpleLineIcons"
+import Icon from "react-native-vector-icons/SimpleLineIcons";
 
 class MenuBar extends Component {
   render() {
-    console.log(this.props)
+    console.log(this.props);
     return (
       <TailwindProvider>
-        <View className="Flex p-1 flex-row justify-around items-center absolute bottom-0 left-0 w-full bg-white shadow-sm">
-            
-        <Buttons icons="home" color="#000" name="Home" active={this.props.active == 1} onPress={()=> this.props.event[1]}/>
-        <Buttons icons="clock" color="#000" name="History" active={this.props.active == 2} onPress={()=> this.props.event[2]}/>
-        <Buttons icons="user" color="#000" name="Akun" active={this.props.active == 3} onPress={()=> this.props.event[3]}/>
-            
+        <View className="flex p-1 flex-row justify-around items-center absolute bottom-0 left-0 w-full bg-white shadow-sm">
+          <Buttons
+            icons="home"
+            color="#000"
+            name="Home"
+            active={this.props.active == 1}
+            onPress={() => this.props.event[1]}
+          />
+          <Buttons
+            icons="clock"
+            color="#000"
+            name="History"
+            active={this.props.active == 2}
+            onPress={() => this.props.event[2]}
+          />
+          <Buttons
+            icons="user"
+            color="#000"
+            name="Akun"
+            active={this.props.active == 3}
+            onPress={() => this.props.event[3]}
+          />
         </View>
       </TailwindProvider>
     );
   }
 }
 
-function Buttons(props){
-    let classes = "flex-1 px-2 py-2 rounded-md bg-white shadow-sm items-center justify-center";
+function Buttons(props) {
+  let classes =
+    "flex-1 px-2 py-2 rounded-md bg-white shadow-sm items-center justify-center";
 
-    if(props.active){
-        classes += " bg-blue-100";
-    }
+  if (props.active) {
+    classes += " bg-blue-100";
+  }
 
-    return(
-
-        <Pressable className={classes}>
-            <Icon name={props.icons} size={24} color={props.active ? "#3784DE" : "#000"}/>
-            <Text className={props.active ? "text-[#3784DE]" : "text-[#000]"}>{props.name}</Text>
-        </Pressable>
-    )
+  return (
+    <TailwindProvider>
+      <Pressable className={classes}>
+        <Icon
+          name={props.icons}
+          size={24}
+          color={props.active ? "#3784DE" : "#000"}
+        />
+        <Text className={props.active ? "text-[#3784DE]" : "text-[#000]"}>
+          {props.name}
+        </Text>
+      </Pressable>
+    </TailwindProvider>
+  );
 }
 
 export default MenuBar;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,21 +1,64 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icons from "react-native-vector-icons/Ionicons"
-import HomeScreen from './HomeScreen';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icons from "react-native-vector-icons/Ionicons";
+import { TailwindProvider } from "tailwindcss-react-native";
+import HomeScreen from "./HomeScreen";
 
 function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
+    <TailwindProvider>
+      <View
+        style={{
+          padding: 32,
+          paddingTop: 96,
+        }}
+      >
+        <Text className="text-2xl font-semibold text-slate-700 mb-8">
+          Riwayat Pembelian
+        </Text>
+        {/* START: PURCHASE HISTORY */}
+        <View className="flex flex-row justify-between py-5 px-6 border-2 border-slate-600 rounded-lg w-full mb-5">
+          <View>
+            <Text className="text-xl font-semibold text-slate-700 mb-3">
+              Mobil
+            </Text>
+            <Text className="text-xl text-slate-700">4 Tiket</Text>
+          </View>
+          <View>
+            <Text className="text-xl text-sky-500 mb-3 text-right">
+              Rp60.000
+            </Text>
+            <Text className="text-xl text-slate-700 text-right">Gopay</Text>
+          </View>
+        </View>
+        {/* END: PURCHASE HISTORY */}
+
+        {/* START: PURCHASE HISTORY */}
+        <View className="flex flex-row justify-between py-5 px-6 border-2 border-slate-600 rounded-lg w-full">
+          <View>
+            <Text className="text-xl font-semibold text-slate-700 mb-3">
+              Trampoline
+            </Text>
+            <Text className="text-xl text-slate-700">4 Tiket</Text>
+          </View>
+          <View>
+            <Text className="text-xl text-sky-500 mb-3 text-right">
+              Rp60.000
+            </Text>
+            <Text className="text-xl text-slate-700 text-right">BRI</Text>
+          </View>
+        </View>
+        {/* END: PURCHASE HISTORY */}
+      </View>
+    </TailwindProvider>
   );
 }
 
 function UserScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>User!</Text>
     </View>
   );
@@ -31,21 +74,19 @@ export default function App() {
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'home-sharp'
-                : 'home-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-time-sharp' : 'ios-time-outline';
-            }else if(route.name === 'user'){
-              iconName = focused ? 'ios-person-sharp' : 'ios-person-outline';
+            if (route.name === "Home") {
+              iconName = focused ? "home-sharp" : "home-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-time-sharp" : "ios-time-outline";
+            } else if (route.name === "user") {
+              iconName = focused ? "ios-person-sharp" : "ios-person-outline";
             }
 
             return <Icons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: '#3784DE',
-          tabBarActiveBackgroundColor: '#D4E7FD',
-          tabBarInactiveTintColor: 'gray',
+          tabBarActiveTintColor: "#3784DE",
+          tabBarActiveBackgroundColor: "#D4E7FD",
+          tabBarInactiveTintColor: "gray",
           headerShown: false,
         })}
       >
